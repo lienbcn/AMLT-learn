f5287d350dba2e15b3f7307c974b0d1c3233d6b0
bejar <bejar@lsi.upc.edu>
2015-09-07 07:20:33 +0200

    Updatinv version number

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 
 setup(
     name='AMLT-learn',
-    version='0.1',
+    version='0.1.1',
     packages=['amltlearn', 'amltlearn.cluster', 'amltlearn.metrics', 'amltlearn.datasets', 'amltlearn.preprocessing',
               'amltlearn.feature_selection', 'amltlearn.feature_selection.unsupervised'],
     url='http://www.cs.upc.edu/~bejar/amlt/amlt.html',
     license='GPL 3.0',
     author='bejar',
     author_email='bejar@lsi.upc.edu',
-    description='Python classed for the AMLT course of the Master in Artificial Intelligence (UPC)'
+    description='Python classes for the AMLT course of the Master in Artificial Intelligence (UPC)'
 )
